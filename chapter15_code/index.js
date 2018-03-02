6f51c553142c1c23144c1efa46972e1fe0712143
perry.z.chen@pwc.com <chenzhipeng901105@gmail.com>
2018-03-02 15:17:42 +0800

    clean code

--- chapter15_code/index.js
+++ chapter15_code/index.js
@@ -17,7 +17,14 @@ function getElements(/*ids...*/) {
 
 function getElementByTagName() {
   var spans = document.getElementsByTagName("span");
-  console.log("spans is " + spans[0]);
+  // console.log("spans is " + spans[0]);
+
+  var parentNode = parent(spans[0], 2);
+
+  var pnode = document.getElementById("ele1");
+  console.log(pnode);
+  var siblingNode = sibling(pnode, 1);
+  console.log("node is " + siblingNode);
   // spans is [object HTMLSpanElement]
 }
 
@@ -28,6 +35,40 @@ function getElementsByClassName() {
 }
 
 function getChildNode() {
-  var element = document.childNodes[0].childNodes[1].childNodes[0];
+  // var element = document.childNodes[0].childNodes[1].childNodes[0];
+  var element = document.firstChild.firstChild.nextSibling.nodeType;
   console.log(element);
+}
+
+function parent(e, n) {
+  if (n === undefined) { n = 1 }
+  while(n-- && e) e = e.parentNode;
+  if (!e || e.nodeType !== 1) { return null; }
+  return e
+}
+
+function sibling(e, n) {
+  console.log(e);
+  while(e && n !== 0) {
+    // console.log(e);
+    if (n > 0) {
+      if (e.nextElementSibling) { 
+        e = e.nextElementSibling 
+      } else {
+        for (e = e.nextSibling; e && e.nodeType !== 1; e = e.nextSibling) {
+        }
+      }
+     n--;
+    }  else {
+      if (e.previousElementSibling) {
+        e = e.previousElementSibling;
+      } else {
+        for ( e = e.previousSibling; e && e.nodeType !== 1; e = e.previousSibling) {
+
+        }
+      }
+       n++;
+    }
+    return e;
+  }
 }
