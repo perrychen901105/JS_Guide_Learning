8aa422ac5784ecf0afdda2999d2c56f27bc12776
perry.z.chen@pwc.com <chenzhipeng901105@gmail.com>
2018-03-02 18:02:35 +0800

    add code

--- chapter15_code/index.js
+++ chapter15_code/index.js
@@ -1,4 +1,3 @@
-`use strict`
 function getElements(/*ids...*/) {
   var elements = {};
   for(var i = 0; i < arguments.length; i++) {
@@ -16,15 +15,19 @@ function getElements(/*ids...*/) {
 }
 
 function getElementByTagName() {
-  var spans = document.getElementsByTagName("span");
+  var spans = document.getElementsByTagName("i");
   // console.log("spans is " + spans[0]);
 
   var parentNode = parent(spans[0], 2);
 
-  var pnode = document.getElementById("ele1");
-  console.log(pnode);
+  var pnode = document.getElementById("ele2");
+  // console.log(pnode);
   var siblingNode = sibling(pnode, 1);
-  console.log("node is " + siblingNode);
+
+  var childNode = document.getElementById("ele2");
+  var actualNode = child(childNode, 0);
+
+  getBoxSize(spans[0]);
   // spans is [object HTMLSpanElement]
 }
 
@@ -48,9 +51,7 @@ function parent(e, n) {
 }
 
 function sibling(e, n) {
-  console.log(e);
   while(e && n !== 0) {
-    // console.log(e);
     if (n > 0) {
       if (e.nextElementSibling) { 
         e = e.nextElementSibling 
@@ -71,4 +72,71 @@ function sibling(e, n) {
     }
     return e;
   }
+}
+
+function child(e, n) {
+  if (e.children) {
+    if (n < 0) { n += e.children.length; }
+    if (n < 0) { return null; }
+    return e.children[n];
+  }
+
+  if (n >= 0) {
+    if (e.firstElementChild) { e = e.firstElementChild } else {
+      for ( e = e.firstChild; e && e.nodeType !== 1; e = e.nextSibling) {}
+    }
+    return sibling(e, n);
+  } else {
+    if (e.lastElementChild) { e = e.lastElementChild; } else {
+      for (e = e.lastChild; e && e.nodeType !== 1; e = e.previousSibling) {}
+    }
+    return sibling(e, n+1);
+  }
+}
+
+function getScrollOffsets(w) {
+  w = w || window;
+
+  // 除了IE 8及更早版本以外，其他浏览器都能用
+  if (w.pageOffset != null) {
+    return {x: w.pageXOffset, y: w.pageYOffset};
+  }
+  // 对标准模式下的IE （或任何浏览器）
+  var d = w.document;
+  if (document.compatMode == "CSS1Compat") {
+    return {x: d.documentElement.scrollLeft, y:d.documentElement.scrollTop};
+  }
+
+  // 对怪异模式下的浏览器
+  return {x: d.body.scrollLeft, y: d.body.scrollTop };
+}
+
+function getViewportSize(w) {
+  w = w || window;
+
+  if (w.innerWidth != null) { 
+    return {w: w.innerWidth, h: w.innerHeight};
+  }
+
+  // 标准模式下的IE
+  var d = w.document;
+  if (document.compatMode == "CSS1Compat") {
+    return { w: d.documentElement.clientWidth, h: d.documentElement.clientHeight};
+  }
+
+  return { w: d.body.clientWidth, h: d.body.clientHeight };
+}
+
+function getBoxSize(e) {
+  // 内联元素用getClientRects，返回数组
+  var box = e.getClientRects();//e.getBoundingClientRects
+  console.log(box);
+  var offsets = getScrollOffsets();
+  var x = box.left + offsets.x;
+  var y = box.top + offsets.y;
+
+  var w = box.width || (box.right - box.left)
+  var h = box.height || (box.bottom - box.top)
+
+  console.log("x " + x + " y " + y + " w " + w + " h " + h);
 }
